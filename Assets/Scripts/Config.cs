d4b51d9d7c64920bcdc8ba7de687cc5054d01606
ardaerbaharli <ardaerbaharli1@gmail.com>
2022-06-09 03:39:37 +0300

    created localization, main page, settings page, backgrounds page

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class Config : MonoBehaviour
+{
+    public static string ActiveBackgroundName
+    {
+        get => PlayerPrefs.GetString("ActiveBackgroundName", "bg0");
+        set => PlayerPrefs.SetString("ActiveBackgroundName", value);
+    }
+
+    public static bool IsVolumeOn
+    {
+        get => PlayerPrefsX.GetBool("IsVolumeOn", true);
+        set => PlayerPrefsX.SetBool("IsVolumeOn", value);
+    }
+
+    public static bool IsVibrationOn
+    {
+        get => PlayerPrefsX.GetBool("IsVibrationOn", true);
+        set => PlayerPrefsX.SetBool("IsVibrationOn", value);
+    }
+
+    public static string ActiveLanguage
+    {
+        get => PlayerPrefs.GetString("ActiveLanguage", "en");
+        set => PlayerPrefs.SetString("ActiveLanguage", value);
+    }
+
+    public static void LoadLocale(string languageIdentifier)
+    {
+       LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales.First(x=>x.Identifier.Code == languageIdentifier);
+    }
+}
